5db9eba04a9e70927484fedca606ee861c082b5d
sobhan khorshidi <sobhankhorshidi1397@gmail.com>
2024-03-26 03:36:15 +0330

    v6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
 
-    <title>o3am</title>
+    <title>همه خدمات</title>
 </head>
 <body>
     <div class="container mt-5">
